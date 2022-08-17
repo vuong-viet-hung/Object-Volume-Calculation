b19fde69bca8c0504971c422aa93102859de461c
vuong-viet-hung <vuongviethung156@gmail.com>
2022-08-17 08:48:22 +0700

    Add metrics logging

--- training/training.py
+++ training/training.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import torch
 
 
@@ -7,7 +9,13 @@ def train(model: torch.nn.Module,
           compute_loss,
           optimizer,
           metrics: dict,
-          n_epochs: int):
+          n_epochs: int,
+          patience: int,
+          saved_model_path: Path,
+          ):
+
+    valid_losses = []
+
     for epoch in range(1, n_epochs + 1):
         # Training mode
         model.train()
@@ -19,7 +27,12 @@ def train(model: torch.nn.Module,
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-            print(f"{train_loss = }")
+            for name, metric in metrics.items():
+                print(f"{name}: {metric(predicted_masks, true_masks)}")
+            print("Accuracy:",
+                  torch.sum(torch.round(predicted_masks) == torch.round(true_masks)) / torch.numel(predicted_masks)
+                  )
+            torch.save(model, str(saved_model_path))
 
         # Validation mode
         model.eval()
@@ -29,12 +42,24 @@ def train(model: torch.nn.Module,
                 predicted_masks = model(images)
                 loss = compute_loss(predicted_masks, true_masks)
                 valid_loss += loss.item()
-                print(f"{valid_loss = }")
+                for name, metric in metrics.items():
+                    print(f"{name}: {metric(predicted_masks, true_masks)}")
+                print(
+                    "Accuracy:",
+                    torch.sum(torch.round(predicted_masks) == torch.round(true_masks)) / torch.numel(predicted_masks),
+                )
 
         train_loss /= len(train_loader)
         valid_loss /= len(valid_loader)
 
         print(f"Epoch: {epoch}/{n_epochs})")
+        print(f"{train_loss = }")
+        print(f"{valid_loss = }")
 
         # for name, metric in metrics.items():
         #     print(f"{name} = {metrics}")
+
+        valid_losses.append(valid_loss)
+        min_valid_losses_idx = valid_losses.index(min(valid_losses))
+        if len(valid_losses) - min_valid_losses_idx > patience:
+            break
